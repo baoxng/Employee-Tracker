0eedae578c42b9340b23546e0059c49622290753
Bao Xiong <bao.xiong2011@gmail.com>
2021-04-14 18:04:04 -0500

    Added dependencies and port connection started to mysql.

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 //Dependencies
-
+const inquirer= require('inquirer');
+const mysql= require('mysql');
+const cTable= require('console.table');
 
 //Port connection
+const connection= mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password:"0N!yme14",
+    database:"employee_trackerDB"
+});
 
-
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected as id " + connection.threadId);
+    intializePrompt();
+  });
 
 //Prompt question
 
