ea230abd8ff644b7395ce8029235ae472a063e8e
Bao Xiong <bao.xiong2011@gmail.com>
2021-04-14 18:59:36 -0500

    Added initial prompt for application.

--- server.js
+++ server.js
@@ -20,6 +20,86 @@ connection.connect(function(err) {
 
 //Prompt question
 
+function intializePrompt(){
+    inquirer.prompt(
+    {
+        type: "list",
+        message: "Welcome to the employee database! What would you like to do?",
+        name: "choice",
+        choices: [
+            "View All Employees",
+            "View All Department",
+            "View All Roles",
+            "View Employee By Manager",
+            "Add Employee",
+            "Add Deparment",
+            "Add Role",
+            "Update Employee Role",
+            "Update Employee Manager",
+            "Delete Employee",
+            "Delete Role",
+            "Delete Department",
+            "View Total Utilized Budget",
+            "Exit"
+        ]
+    }).then(function(answer){
+        switch(answer.choice){
+            case "View All Employees":
+                viewAllEmployees();
+                break;  
+            case "View All Department":
+                viewAllDept();
+                break;
+            case "View All Roles":
+                viewAllRole();
+                break;
+            case "View Employee By Manager":
+                viewByManager();
+                break;
+            case "Add Employee":
+                addEmployee();
+                break;
+            case "Add Deparment":
+                addDepartment();
+                break;
+            case "Add Role":
+                addRole();
+                break;
+            case "Update Employee Role":
+                updateEmployeeRole();
+                break;
+            case "Update Employee Manager":
+                updateEmployeeManager();
+                break;
+            case "Delete Employee":
+                deleteEmployee();
+                break;
+            case "Delete Role":
+                deleteRole();
+                break;
+            case "Delete Department":
+                deleteDepartment();
+                break;
+            case "View Total Utilized Budget":
+                totalUtilizedBudget()
+                break;
+            case "Exit":
+                exitApp();
+                break
+            default:
+                break;
+        }
+
+    }).catch(err =>{
+        if(err){
+            console.log("Please try again!")
+        }else{
+            console.log("Looks good!")
+        }
+    });
+
+}
+
 
 //Add departments, role, employees
 
