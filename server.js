68a2d9e863aad6ad27d318c8c524260884a3a2f9
Bao Xiong <bao.xiong2011@gmail.com>
2021-04-14 17:55:34 -0500

    Pseudo coded requirement for application.

---
+++ server.js
@@ -0,0 +1,30 @@
+//Dependencies
+
+
+//Port connection
+
+
+
+//Prompt question
+
+
+//Add departments, role, employees
+
+
+//View departments, roles, employees
+
+
+//Update employee roles
+
+
+//Update employee managers
+
+
+//View employees by manager
+
+
+//Delete departments, roles and employees
+
+
+//View the total utilized budget of a department
+
