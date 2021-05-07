57b12751be6e860b2b996461ef4e0260f9dde75e
Bao Xiong <bao.xiong2011@gmail.com>
2021-05-07 15:14:59 -0500

    Logic added to functions.

--- server.js
+++ server.js
@@ -53,9 +53,6 @@ function intializePrompt(){
             case "View All Roles":
                 viewAllRole();
                 break;
-            case "View Employee By Manager":
-                viewByManager();
-                break;
             case "Add Employee":
                 addEmployee();
                 break;
@@ -79,9 +76,6 @@ function intializePrompt(){
                 break;
             case "Delete Department":
                 deleteDepartment();
-                break;
-            case "View Total Utilized Budget":
-                totalUtilizedBudget()
                 break;
             case "Exit":
                 exitApp();
@@ -144,35 +138,132 @@ function addEmployee(){
 }
 
 function addDepartment(){
-
+    inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "What Department is being added?"
+        }
+    ]).then(function(res){
+        const query= connection.query(
+            "INSERT INTO department SET ? ",
+            {
+                name: res.name
+            },
+            (err)=>{
+                if(err) throw err
+                console.table(res);
+                intializePrompt();
+            }
+        )
+    })
 }
 
 function addRole(){
-
+    connection.query("SELECT role.title AS Title, rolse.salary AS Salary FROM role", (err, res)=> [
+        inquirer.prompt([
+            {
+                name: "Title",
+                type: "input",
+                message: "What is the new role titled?"
+            },
+            {
+                name: "Salary",
+                type: "input",
+                message: "What is the salary?"
+            }
+        ]).then((res)=>{
+            connection.query(
+                "INSERT INTO role SET ?",
+                {
+                    title: res.Title,
+                    salary: res.Salary,
+                },
+                (err) => {
+                    if(err) throw err;
+                    console.table(res);
+                    intializePrompt();
+                }
+            )
+        })
+    ])
 }
 
-//View departments, roles, employees, employees by manager
+//View departments, roles, employees
 
 function viewAllEmployees(){
-
+    connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;", 
+    (err, res) =>{
+      if (err) throw err
+      console.table(res)
+      intializePrompt()
+  })
 }
 
 function viewAllDept(){
-
+    connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", 
+    (err, res) =>{
+      if (err) throw err
+      console.table(res)
+      intializePrompt()
+    })
 }
 
 function viewAllRole(){
-
-}
-
-function viewByManager(){
-
+    connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
+    (err, res) => {
+    if (err) throw err
+    console.table(res)
+    intializePrompt()
+    })
 }
 
 //Update employee roles, employee managers
 
 function updateEmployeeRole(){
-
+    connection.query("SELECT employee.last_name, role.title FROM employee JOIN role ON employee.role_id = role.id;",
+        (err, res) =>{
+        // console.log(res)
+         if (err) throw err
+         console.log(res)
+        inquirer.prompt([
+              {
+                name: "lastName",
+                type: "list",
+                choices: function() {
+                  const lastName = [];
+                  for (let i = 0; i < res.length; i++) {
+                    lastName.push(res[i].last_name);
+                  }
+                  return lastName;
+                },
+                message: "What is the Employee's last name? ",
+              },
+              {
+                name: "role",
+                type: "list",
+                message: "What is the Employees new title? ",
+                choices: selectRole()
+              },
+          ]).then(function(value) {
+            const roleId = selectRole().indexOf(value.role) + 1
+            connection.query("UPDATE employee SET WHERE ?", 
+            {
+              last_name: val.lastName
+               
+            }, 
+            {
+              role_id: roleId
+               
+            }, 
+            (err)=>{
+                if (err) throw err
+                console.table(val)
+                intializePrompt()
+            })
+      
+        });
+      });
 }
 
 function updateEmployeeManager(){
@@ -193,14 +284,35 @@ function deleteDepartment(){
 
 }
 
-//View the total utilized budget of a department
-
-function totalUtilizedBudget(){
-
-}
 
 //Exit Application
 
 function exitApp(){
 
+}
+
+
+//Other Functions
+
+const roleArr=[];
+function selectRole(){
+    connection.query("SELECT * FROM role", (err, res) => {
+        if(err) throw err
+        for(let i=0; i < res.length; i++ ){
+            roleArr.push(res[i].title);
+        }
+    })
+    return roleArr;
+}
+
+const managerArr=[]
+function selectManager(){
+    connection.query("SELECT first_name, last_name FROM employee WHERE manager_id IS NULL", function(err, res) {
+        if (err) throw err
+        for (let i = 0; i < res.length; i++) {
+          managerArr.push(res[i].first_name);
+        }
+    
+      })
+      return managersArr;
 }
